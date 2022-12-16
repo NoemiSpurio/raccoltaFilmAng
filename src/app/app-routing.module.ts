9f1191fe9395deda55a8f86d0fe923dcf78f9d7d
NoemiSpurio <noemi.spurio@gmail.com>
2022-12-16 12:13:32 +0100

    aggiunta la AuthGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './core/auth/auth.guard';
 
 const routes: Routes = [
   {
     path: 'welcome',
     loadChildren: () => import('./features/welcome/welcome.module').then(m => m.WelcomeModule),
+    canActivate: [AuthGuard]
   },
   {
     path: 'regista',
     loadChildren: () => import('./features/regista/regista.module').then(m => m.RegistaModule),
+    canActivate: [AuthGuard]
   },
   {
     path: 'film',
     loadChildren: () => import('./features/film/film.module').then(m => m.FilmModule),
+    canActivate: [AuthGuard]
   },
   {
     path: 'login',
